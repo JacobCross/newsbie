b542d80105b035e9c96cd382da8d302d90700ab1
Jacob Cross <JakeCross1997@gmail.com>
2016-09-29 11:32:17 -0700

    Delete unnecessary files

--- apikey_test.py
+++
@@ -1,7 +0,0 @@
-from newsbie_config import *
-
-print(API_KEY)
-
-base_url = "http://gateway-a.watsonplatform.net/calls/url/URLGetCombinedData?extract=page-image,concepts,entity,keyword,taxonomy&apikey="+API_KEY+"&showSourceText=0&sentiment=1&quotations=1&outputMode=json&url="
-
-print(base_url)
